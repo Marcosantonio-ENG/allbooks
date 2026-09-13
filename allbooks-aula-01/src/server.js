11f184def625a80f92b0cf4530c47681767b561f
Marcos Antonio <marcoosilva@hotmail.com.br>
2026-09-13 18:02:27 -0300

    Altera o Server

--- allbooks-aula-01/src/server.js
+++ allbooks-aula-01/src/server.js
@@ -18,10 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
   console.log("Boas-vindas a API do AllBooks!")
-  console.log("API disponível através da url http://localhost:8000")
-=======
-  console.log("Acesse essa API em http://localhost:8000")
->>>>>>> b68ab3315cf56a2032edf9e70e5c36e3fa923c33
+  console.log("API disponível em http://localhost:8000")
 })
